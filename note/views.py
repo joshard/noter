3c9e04dd9d4de955d341d832b567f0050e77ca7f
Joshua Dhikusooka <30894937+joshard@users.noreply.github.com>
2024-10-07 10:13:10 +0300

    Update views.py

--- note/views.py
+++ note/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
-from .models import Note
-from django.contrib.auth import authenticate, login, logout
+from .models import Note, User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login as login_process, logout as logout_process
 # Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
@@ -28,29 +29,33 @@ def noteCreate(request):
 
 def signup(request):
     if request.method == "POST":
-        title = request.POST.get("title")
-        content = request.POST.get("content")
-        createNote = Note.objects.create(title=title, content=content)
-        createNote.save()
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, "login.html")
+        else:
+            form = UserCreationForm()
+            return render(request, "signup.html", {'form': form})
+        
     else:
-        notes = Note.objects.all()
-    context = {"Notes": notes}
-    return render(request, "login.html", context)
+        form = UserCreationForm()
+    return render(request, "signup.html", {'form': form})
 
-def login_view(request):
+def login(request):
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("pwd")
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request, user)
+            login_process(request, user)
             notes = Note.objects.all()
             context = {"Notes": notes}
             return render(request, "index.html", context)
+        # else:
+        #     return render(request, "login.html")
     else:
-        
         return render(request, "login.html")
 
 def logout_view(request):
-    logout(request)
-    return render(request, "login.html")
+    logout_process(request)
+    return redirect('login')
