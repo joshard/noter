8bd785227221d38bcf16ca412657e422c5a20924
Joshua Dhikusooka <30894937+joshard@users.noreply.github.com>
2024-10-07 10:09:37 +0300

    Update urls.py

--- note/urls.py
+++ note/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("noteCreate", views.noteCreate, name="noteCreate"),
     path("signup", views.signup, name="signup"),
-    path("login", views.login_view, name="login"),
+    path("login/", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
 ]
